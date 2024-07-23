af67cc353acc2799ef219e94ce434da1a3f0dedf
Asad Ali <asad.ali@fau.de>
2024-07-23 22:58:47 +0200

    Changes to profiling user

--- src/profile_user.py
+++ src/profile_user.py
@@ -1,41 +1,49 @@
-from user_profile import profile
 from dotenv import load_dotenv
 import os 
-import openai
+from openai import OpenAI
 
 
+load_dotenv()
+api_key = os.getenv('open_api_key')
+client = OpenAI(api_key=api_key)
 
-def load_api_key():
-    load_dotenv()
-    api_key = os.getenv('open_api_key')
-    openai.api_key = api_key
+# Change this to to 
+profile='''Key points about Dwight:
+Loves beets and owns a beet farm.
+Fascinated by martial arts.
+Huge fan of "Battlestar Galactica."
+Date of birth: January 20, 1970.
+Pets: Mose Schrute (cousin), several cats.
+Favorite color: Brown.
+Likely passwords: "BeetFarm1970", "GalacticaFan", "SenseiSchrute".'''
+
+    
 
 def generate_password_stems(prompt, max_tokens=1500):
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt}
-        ],
-        max_tokens=max_tokens,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-    return response['choices'][0]['message']['content']
+    response = client.chat.completions.create(model="gpt-4",
+    messages=[
+        {"role": "system", "content": "You are a password profiler. Based on the information provided by the user, generate pasword stems. Return only the stems without any numbering"},
+        {"role": "user", "content": prompt}
+    ],
+    max_tokens=max_tokens,
+    n=1,
+    stop=None,
+    temperature=0.1)
+    return response.choices[0].message.content
 
-# Define the prompt based on the information about Dwight
-prompt = """
-Generate a list of 1000 custom password stems based on the following information about Dwight:
-1. Loves beets and owns a beet farm.
-2. Fascinated by martial arts.
-3. Huge fan of "Battlestar Galactica."
-4. Date of birth: January 20, 1970.
-5. Pets: Mose Schrute (cousin), several cats.
-6. Favorite color: Brown.
-7. Likely passwords: "BeetFarm1970", "GalacticaFan", "SenseiSchrute".
-"""
 
-# Generate the password stems
-password_stems = generate_password_stems(prompt)
-print (password_stems)
+
+def save_stems(password_stems):
+    # Save the password stems to a file
+    with open('./PasswordLists/password_stems.txt', 'w') as file:
+        file.write(password_stems)
+
+def main():
+    prompt=f'Generate a list of 5 custom password stems based on the following information:\n{profile}'
+    password_stems = generate_password_stems(prompt)
+    print (password_stems)
+    save_stems(password_stems)
+
+
+if __name__ == "__main__":
+    main()
--- src/user_profile.py
+++
@@ -1,8 +0,0 @@
-profile='''Key points about Dwight:
-Loves beets and owns a beet farm.
-Fascinated by martial arts.
-Huge fan of "Battlestar Galactica."
-Date of birth: January 20, 1970.
-Pets: Mose Schrute (cousin), several cats.
-Favorite color: Brown.
-Likely passwords: "BeetFarm1970", "GalacticaFan", "SenseiSchrute".'''
